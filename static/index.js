b320ac1763d9ce2a1c3924e5c1d110d49b033176
leesoonjae <117057864+leesoonjae@users.noreply.github.com>
2022-11-03 09:15:07 +0900

    Add files via upload

---
+++ static/index.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+            show_study()
+        });
+
+        function show_study() {
+            $.ajax({
+                type: "GET",
+                url: "/study",
+                data: {},
+                success: function (response) {
+                    console.log('성공')
+                    let rows = response['good']
+                    for (let i = 0; i < rows.length; i++) {
+                        let name = rows[i]['name']
+                        let comment = rows[i]['comment']
+                        let num = rows[i]['num']
+                        let done = rows[i]['done']
+
+                        let temp_html = ``
+                        if (done == 0) {
+                            temp_html = `<li>
+                                    <h2>${name}</h2>
+                                    <h2>✅ ${comment}</h2>
+                                    <button onclick="done_study(${num})" type="button" class="btn btn-outline-primary">제거</button>
+                                </li>`
+                        }
+                        $('#comment-list').append(temp_html)
+                    }
+                }
+            });
+        }
+
+        function save_study() {
+            let name = $('#name').val()
+            let comment = $('#comment').val()
+
+            $.ajax({
+                type: "POST",
+                url: "/study",
+                data: {name_give: name, comment_give: comment},
+                success: function (response) {
+                    alert(response["msg"])
+                    window.location.reload()
+                }
+            });
+        }
+
+        function done_study(num) {
+            $.ajax({
+                type: "POST",
+                url: "/study/done",
+                data: {num_give: num},
+                success: function (response) {
+                    alert(response["msg"])
+                    window.location.reload()
+                }
+            });
+        }
