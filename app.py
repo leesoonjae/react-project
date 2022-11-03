31e1f3cc070b61b835647bee12f4eec20fdb3b8e
leesoonjae <117057864+leesoonjae@users.noreply.github.com>
2022-11-03 19:38:39 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,49 +1,66 @@
 from flask import Flask, render_template, request, jsonify
+
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.2ko6qez.mongodb.net/Cluster0?retryWrites=true&w=majority')
+
+client = MongoClient('mongodb+srv://junho:test@cluster0.zlxkvp3.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
+
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
+
+
+@app.route('/junho')
+def introduce_jun():
+    return render_template('junho.html')
+
 @app.route('/soon')
-def introduce_index():
-   return render_template('soon.html')
+def introduce_soon():
+    return render_template('soon.html')
 
 
-@app.route("/study", methods=["POST"])
-def study_post():
-    name_receive = request.form['name_give']
+
+
+@app.route("/comment", methods=["POST"])
+def comment_post():
     comment_receive = request.form['comment_give']
-    teamName_receive = request.form['teamName_give']
+    writer_receive = request.form['writer_give']
+    soonjae_receive = request.form['soonjae_give']
 
-    comment_list = list(db.study.find({}, {'_id': False}))
+    comment_list = list(db.comment.find({}, {'_id': False}))
     count = len(comment_list) + 1
 
     doc = {
-        'num':count,
-        'name':name_receive,
-        'comment':comment_receive,
-        'done':0,
-        'teamName': teamName_receive,
-        'soon': 0
+        'num': count,
+        'comment': comment_receive,
+        'del': 0,
+        'writer': writer_receive,
+        'name': 0,
+        'soonjae': soonjae_receive
     }
-    db.study.insert_one(doc)
+    db.comment.insert_one(doc)
 
-    return jsonify({'msg':'댓글 입력'})
+    return jsonify({'msg': '댓글 완료!'})
 
-@app.route("/study/done", methods=["POST"])
-def study_done():
+
+@app.route("/comment/del", methods=["POST"])
+def comment_del():
     num_receive = request.form['num_give']
-    db.study.update_one({'num': int(num_receive)}, {'$set': {'done': 1}})
-    return jsonify({'msg': '입력 완료!'})
 
-@app.route("/study", methods=["GET"])
-def study_get():
-    comment_list = list(db.study.find({}, {'_id': False}))
-    return jsonify({'good':comment_list})
+    db.comment.update_one({'num': int(num_receive)}, {'$set': {'del': 1}})
+
+    return jsonify({'msg': '삭제 완료!'})
+
+
+@app.route("/comment", methods=["GET"])
+def comment_get():
+    comment_list = list(db.comment.find({}, {'_id': False}))
+
+    return jsonify({'comments': comment_list})
+
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
